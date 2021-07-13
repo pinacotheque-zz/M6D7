97b4ef861883321257d7219f042db4aa8228091d
Gamze Güneri <gamzegunerig@gmail.com>
2021-07-13 18:05:20 +0300

    added index.js

--- src/server.js
+++ src/server.js
@@ -4,17 +4,17 @@ import cors from "cors";
 import authorsRouter from "./services/authors/index.js";
 import blogsRouter from "./services/blogs/index.js";
 
-const server = express();
+const app = express();
 
 const port = process.env.PORT || 3009;
 
-server.use(cors());
+app.use(cors());
 
-server.use(express.json());
-server.use("/authors", authorsRouter);
-server.use("/blogs", blogsRouter);
+app.use(express.json());
+app.use("/authors", authorsRouter);
+app.use("/blogs", blogsRouter);
 
 
-server.listen(port, () => console.log("Server is running on port ", port));
+app.listen(port, () => console.log("Server is running on port ", port));
 
-server.on("error", (error) => console.log("Something went wrong.", error));
+app.on("error", (error) => console.log("Something went wrong.", error));
