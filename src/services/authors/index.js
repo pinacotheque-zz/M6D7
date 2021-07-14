d5ff8d94f10a68c3e1246376ee512b916f94bfa0
Gamze Güneri <gamzegunerig@gmail.com>
2021-07-14 16:08:22 +0300

    needs test

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,35 +1,56 @@
+import express from 'express'
+import db from '../../utils/db/index.js'
+import createError from 'http-errors'
 import { Router } from 'express'
 
-const router = Router()
+const authorsRouter = express.Router()
 
-router
-  .route("/")
-  .get(async (req, res, next) => {
+authorsRouter.route("/")
+.get(async (req, res, next) => {
     try {
+        const query = " SELECT * FROM author ORDER BY created_at DESC"
+        const data = await db.query(query)
+        res.send(data.rows)
     } catch (error) {
       console.log(error);
       next(error);
     }
   })
-  .post(async (req, res, next) => {
+
+
+.post(async (req, res, next) => {
     try {
+        let { name, surname, avatar } = req.body
+        const query = `INSERT INTO (name, surname, avatar) VALUES ('${name}', '${surname}, '${avatar}') RETURNING * `
+        const data = await db.query(query)
+        res.send(data.rows[0])
     } catch (error) {
       console.log(error);
       next(error);
     }
   });
 
-router
-  .route("/:id")
-  .get(async (req, res, next) => {
+authorsRouter.route("/:id")
+.get(async (req, res, next) => {
     try {
+        const query = ` SELECT * FROM author WHERE id = ${req.params.id} `
+        const data = await db.query(query)
+        res.send(data.rows[0])
     } catch (error) {
       console.log(error);
       next(error);
     }
   })
+
   .delete(async (req, res, next) => {
     try {
+        const query = ` DELETE FROM author WHERE id = ${req.params.id} `
+        const data = await db.query(query)
+        if (data.rowCount > 0) {
+            res.send('Author deleted successfully.')
+        } else {
+            res.send('Error occured while deleting.')
+        }
     } catch (error) {
       console.log(error);
       next(error);
@@ -37,6 +58,11 @@ router
   })
   .put(async (req, res, next) => {
     try {
+        let { name, surname, avatar } = req.body
+        const fields = Object.keys(req.body).map(key =>`${key} = '$req.body[key]'`).join(',')
+        const query = ` UPDATE author SET ${fields} WHERE id = ${req.params.id} RETURNING * `
+        const data = await db.query(query)
+        res.send(data.rows[0])
     } catch (error) {
       console.log(error);
       next(error);
@@ -44,4 +70,4 @@ router
   });
 
 
-export default router
+export default authorsRouter
